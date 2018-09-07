9d375486d579b239e209ae9d38cd4064b3aa2aa2
Randy Mikkelsaar <randy.mikkelsaar@gmail.com>
2018-09-07 09:42:08 -0400

    Initial grafite starter

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'permissions' => \App\Http\Middleware\Permissions::class,
+        'roles' => \App\Http\Middleware\Roles::class
     ];
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/dashboard') }}">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -78,7 +78,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ url('/dashboard') }}">Dashboard</a>
+                        @else
+                            Laravel
+                        @endauth
+                    @endif
+
                 </div>
 
                 <div class="links">
