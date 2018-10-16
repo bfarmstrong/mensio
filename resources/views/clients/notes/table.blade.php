4d98b1ff59497c1790615479ecde7ca053311bde
Dylan Aspden <d.aspden@coretec.io>
2018-10-16 20:35:41 +0000

    Merged PR 115: Ticket 365: Segregate notes into two tabs, final and drafts
    Related work items: #365

--- resources/views/clients/notes/table.blade.php
+++ resources/views/clients/notes/table.blade.php
@@ -1,35 +1,76 @@
-<table class="table table-hover table-outline table-striped">
-        <thead class="thead-light">
-            <tr>
-                <th>@lang('clients.notes.table.creator')</th>
-                <th>@lang('clients.notes.table.date')</th>
-                <th>@lang('clients.notes.table.status')</th>
-                <th>@lang('clients.notes.table.actions')</th>
-            </tr>
-        </thead>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a data-toggle="tab" class="nav-link active" href="#final-notes">Final Notes</a>
+  </li>
+  <li class="nav-item">
+    <a data-toggle="tab" class="nav-link" href="#drafts">Drafts</a>
+  </li>
+</ul>
+<div class="tab-content">
+	<div id="final-notes" class="tab-pane active">
+		<table class="table table-hover table-outline table-striped">
+			<thead class="thead-light">
+				<tr>
+					<th>@lang('clients.notes.table.creator')</th>
+					<th>@lang('clients.notes.table.date')</th>
+					<th>@lang('clients.notes.table.status')</th>
+					<th>@lang('clients.notes.table.actions')</th>
+				</tr>
+			</thead>
 
-        <tbody>
-            @foreach ($notes as $note)
-                <tr>
-                    <td>{{ $note->therapist->name }}</td>
-                    <td>{{ $note->updated_at }}</td>
-                    <td>
-                        @if ($note->is_draft)
-                            @lang('clients.notes.table.draft')
-                        @else
-                            @lang('clients.notes.table.final')
-                        @endif
-                    </td>
-                    <td>
-                        <a
-                            class="btn btn-primary btn-sm"
-                            href="{{ url("clients/$user->id/notes/$note->uuid") }}"
-                        >
-                            <i class="fas fa-search mr-1"></i>
-                            @lang('clients.notes.table.view')
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+			<tbody>
+				@foreach ($notes as $note)
+				@if ($note->is_draft == 0)
+					<tr>
+						<td>{{ $note->therapist->name }}</td>
+						<td>{{ $note->updated_at }}</td>
+						<td>@lang('clients.notes.table.final')</td>
+						<td>
+							<a
+								class="btn btn-primary btn-sm"
+								href="{{ url("clients/$user->id/notes/$note->uuid") }}"
+							>
+								<i class="fas fa-search mr-1"></i>
+								@lang('clients.notes.table.view')
+							</a>
+						</td>
+					</tr>
+				@endif
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+	<div id="drafts" class="tab-pane fade">
+		<table class="table table-hover table-outline table-striped">
+			<thead class="thead-light">
+				<tr>
+					<th>@lang('clients.notes.table.creator')</th>
+					<th>@lang('clients.notes.table.date')</th>
+					<th>@lang('clients.notes.table.status')</th>
+					<th>@lang('clients.notes.table.actions')</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				@foreach ($notes as $note)
+				@if ($note->is_draft)
+					<tr>
+						<td>{{ $note->therapist->name }}</td>
+						<td>{{ $note->updated_at }}</td>
+						<td>@lang('clients.notes.table.draft')</td>
+						<td>
+							<a
+								class="btn btn-primary btn-sm"
+								href="{{ url("clients/$user->id/notes/$note->uuid") }}"
+							>
+								<i class="fas fa-search mr-1"></i>
+								@lang('clients.notes.table.view')
+							</a>
+						</td>
+					</tr>
+				@endif
+				@endforeach
+			</tbody>
+		</table>
+  </div>
+</div>
