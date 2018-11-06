46e587493f3ac69ed1b8d9b0a3d8cfb683c40257
Komal Bhatt <k.bhatt@coretec.io>
2018-11-06 12:16:01 +0530

    Ability to add and removes users from their clinic

--- database/migrations/2018_11_01_120500_add_deleteat_to_users.php
+++ database/migrations/2018_11_01_120500_add_deleteat_to_users.php
@@ -26,7 +26,7 @@ class AddDeleteatToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->softDeletes();
+            $table->dropSoftDeletes();
         });
     }
 }
