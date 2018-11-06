90850d5227934b7f24e36a32cd457ac248d4cfaa
Dylan Aspden <d.aspden@coretec.io>
2018-11-06 16:02:59 -0500

    feat(Receipt): add the receipt model to the database

---
+++ app/Models/Receipt.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\SetsUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * A receipt is an acknowledgement of an appointment.
+ */
+class Receipt extends Model
+{
+    use SetsUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'appointment_date',
+        'clinic_id',
+        'supervisor_id',
+        'therapist_id',
+        'user_id',
+    ];
+
+    /**
+     * The columns that generate a UUID.
+     *
+     * @var array
+     */
+    protected $uuids = ['uuid'];
+
+    /**
+     * A receipt is created for a client within a clinic.
+     *
+     * @return BelongsTo
+     */
+    public function clinic()
+    {
+        return $this->belongsTo(Clinic::class, 'clinic_id');
+    }
+
+    /**
+     * A receipt may be signed by a supervisor.
+     *
+     * @return BelongsTo
+     */
+    public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    /**
+     * A receipt is created by a therapist.
+     *
+     * @return BelongsTo
+     */
+    public function therapist()
+    {
+        return $this->belongsTo(User::class, 'therapist_id');
+    }
+
+    /**
+     * A receipt is attached to a user (client).
+     *
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
