e67da30a380f6eccf1ff54270562a76d793ce527
Dylan Aspden <d.aspden@coretec.io>
2018-11-08 14:12:41 -0500

    fix(Database): make health card number blind index nullable

---
+++ database/migrations/2018_11_08_184542_make_health_card_blind_index_nullable.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Ensure that the health card blind index is allowed to be null.
+ */
+class MakeHealthCardBlindIndexNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE users MODIFY health_card_number_bidx VARCHAR(512)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE users MODIFY health_card_number_bidx VARCHAR(191) NOT NULL;');
+    }
+}
