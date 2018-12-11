a2ebaa56f9c08eb25857b15b8392fc4c17508371
Komal Bhatt <komal@webtechsystem.com>
2018-12-11 17:20:08 +0530

    multiple questionnaires assigned to battery

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Traits\Loggable;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\SetsUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -14,14 +15,21 @@ class Survey extends Model
 {
     // Log any changes to the survey model
     use Loggable;
-
+	use SetsUuids;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = ['description', 'name','user_id'];
-
+	
+    /**
+     * Sets the columns that should have a UUID generated.
+     *
+     * @var array
+     */
+    protected $uuids = ['uuid'];
+	
     /**
      * A survey may have many questionnaires attached to it.
      *
