ba89b27914610466e8978572a1bd9f5cb266e446
Komal Bhatt <komal@webtechsystem.com>
2019-01-16 18:57:47 +0530

    Create Therapist Dashboard

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,10 +2,45 @@
 
 namespace App\Http\Controllers;
 use Notification;
+use App\Enums\Roles;
 use App\Notifications\ConsentEmail;
+use App\Services\Criteria\General\OrderBy;
+use App\Services\Criteria\General\WhereEqual;
+use App\Services\Impl\IUserService;
+use App\Services\Impl\ICommunicationLogService;
+use App\Services\Criteria\General\WhereRelationEqual;
+use App\Services\Criteria\User\WhereCurrentClient;
+use App\Services\Criteria\General\WithRelation;
 
 class PagesController extends Controller
 {
+	 /**
+     * The user service implementation.
+     *
+     * @var IUserService
+     */
+    protected $userService;
+	
+	/**
+     * The communication log service implementation.
+     *
+     * @var ICommunicationLogService
+     */
+    protected $communicationLogService;
+	/**
+     * Creates an instance of `NoteController`.
+     *
+     * @param ICommunicationLogService $communicationLogService
+     * @param IUserService             $userService
+     */
+    public function __construct(
+        ICommunicationLogService $communicationLogService,
+        IUserService $userService
+    ) {
+        $this->communicationLogService = $communicationLogService;
+        $this->userService = $userService;
+    }
+	
     /**
      * Homepage.
      *
@@ -23,11 +58,27 @@ class PagesController extends Controller
      */
     public function dashboard()
     {
+		$clients = $this->userService
+            ->pushCriteria(new WhereRelationEqual('roles', 'level', Roles::Client))
+            ->pushCriteria(new WhereCurrentClient(\Auth::user()->id))
+            ->pushCriteria(new WithRelation('roles'))
+			->all();
+		foreach($clients as $client) {	
+			$communications[$client->id] = $this->communicationLogService
+				//->pushCriteria(new WhereEqual('clinic_id', $request->attributes->get('clinic')->id))
+				->pushCriteria(new WhereEqual('user_id', $client->id))
+				->pushCriteria(new OrderBy('updated_at', 'desc'))
+				->paginate(1, 'communication_page');
+			$client_names[$client->id] = $this->userService->find($client->id);
+		}	
         if (auth()->user()->isAdmin()) {
             return redirect('admin/dashboard');
         }
 
-        return view('dashboard');
+        return view('dashboard')->with([
+            'communications' => $communications,
+            'client_names' => $client_names,
+        ]);
     }
 	public function checkconsent()
 	{
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,4 +4,61 @@
 
 @section('content.breadcrumbs', Breadcrumbs::render('dashboard'))
 @section('content.dashboard')
+@if(Auth::user()->isTherapist())
+<div class="container ">
+    <div id="accordion" class="accordion">
+        <div class="card mb-0">
+            <div class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
+                <a class="card-title">
+                    Your Clients
+                </a>
+            </div>
+            <div id="collapseOne" class="card-body collapse show" data-parent="#accordion" >
+				<div class="row">
+					<div class="col-sm-12">
+					<input class="form-control" id="myInput" type="text" placeholder="Search..">
+					<br>
+                	<table class="table table-striped">
+					<thead>
+					  <tr>
+						<th>Client Name</th>
+						<th>Next Appointment Date</th>
+						<th>Session Notes</th>
+					  </tr>
+					</thead>
+					<tbody id="myTable">
+					@foreach ($communications as $key => $communication)
+						@foreach ($communication as $log) 
+					  <tr>
+						<td>{{ $client_names[$key]->name}}</td>
+						<td>{{ $log->appointment_date }}</td>
+						<td>{{ $log->notes }}</td>
+					  </tr>
+						@endforeach
+					@endforeach
+					</tbody>
+					</table>
+					</div>
+					<!-- <div class="col-sm-2">
+						<button type="button" class="btn btn-primary">Primary</button>
+						<button type="button" class="btn btn-secondary">Secondary</button>
+					</div> -->
+				</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 @endsection
+@push('scripts')
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+@endpush
