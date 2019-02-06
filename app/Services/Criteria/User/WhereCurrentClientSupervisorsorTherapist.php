25e2f79eb52106da5b33db21e31057f11a42d616
kbhattcoretec <45381848+kbhattcoretec@users.noreply.github.com>
2019-02-06 20:05:09 +0530

    Feature/79 (#103)
    * issue with questionner view
    
    * As a Sr. therapist, I should be able to see all the clients of my supervisee

---
+++ app/Services/Criteria/User/WhereCurrentClientSupervisorsorTherapist.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\Criteria\User;
+
+use App\Services\Criteria\Criteria;
+use App\Services\IBaseService;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Criteria which checks that the given client is a therapist of the user.
+ */
+class WhereCurrentClientSupervisorsorTherapist extends Criteria
+{
+    /**
+     * The identifier of the user to ensure the client is currently tied to the
+     * user.
+     *
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * Creates an instance of `WhereCurrentClientSupervisorsorTherapist`.
+     *
+     * @param mixed $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Applies the criteria.
+     *
+     * @param Model        $model
+     * @param IBaseService $service
+     *
+     * @return Model
+     */
+    public function apply($model, IBaseService $service)
+    {
+        $query = $model->whereHas('supervisors', function ($query) {
+            $query->where('therapist_id', $this->id)
+			->orWhere('supervisor_id', $this->id);
+        });
+        return $query;
+    }
+}
